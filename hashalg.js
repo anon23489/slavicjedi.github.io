84e01c307be4cb1a2f5455df48b598aac7bfda37
KaliBoi <52522069+Programming-Biscuit@users.noreply.github.com>
2020-04-26 13:56:43 -0400

    Update hashalg.js

--- hashalg.js
+++ hashalg.js
@@ -1,214 +1,262 @@
+function multiply(numb) {
+    var sum=1;
+    for (var i=0; i<numb.length; i++) {
+        sum = sum * numb[i];
+    } 
+    return sum;
+}
 function hash(array){
   var prehash = [1];
   var hashnum = prehash.length;
   var array2 = array.split('');
   if (array2.includes('a') == true){
     prehash[hashnum] = 82;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('b') == true){
     prehash[hashnum] = 25;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('c') == true){
     prehash[hashnum] = 3;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('d') == true){
     prehash[hashnum] = 96;
-    hashnum = prehash.length;
+    hashnum ++;
   } 
   if (array2.includes('e') == true){
     prehash[hashnum] = 36;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('f') == true){
     prehash[hashnum] = 81;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('g') == true){
     prehash[hashnum] = 54;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('h') == true){
     prehash[hashnum] = 11;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('i') == true){
     prehash[hashnum] = 72;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('j') == true){
     prehash[hashnum] = 89;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('k') == true){
     prehash[hashnum] = 66;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('l') == true){
     prehash[hashnum] = 86;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('m') == true){
     prehash[hashnum] = 489;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('n') == true){
     prehash[hashnum] = 74;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('o') == true){
     prehash[hashnum] = 91;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('p') == true){
     prehash[hashnum] = 50;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('q') == true){
     prehash[hashnum] = 18;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('r') == true){
     prehash[hashnum] = 34;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('s') == true){
     prehash[hashnum] = 48;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('t') == true){
     prehash[hashnum] = 89;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('u') == true){
     prehash[hashnum] = 6;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('v') == true){
     prehash[hashnum] = 42;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('w') == true){
     prehash[hashnum] = 15;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('x') == true){
     prehash[hashnum] = 76;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('y') == true){
     prehash[hashnum] = 92;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('z') == true){
     prehash[hashnum] = 29;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('A') == true){
     prehash[hashnum] = 27;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('B') == true){
     prehash[hashnum] = 45;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('C') == true){
     prehash[hashnum] = 43;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('D') == true){
     prehash[hashnum] = 97;
-    hashnum = prehash.length;
+    hashnum ++;
   } 
   if (array2.includes('E') == true){
     prehash[hashnum] = 63;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('F') == true){
     prehash[hashnum] = 17;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('G') == true){
     prehash[hashnum] = 34;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('H') == true){
     prehash[hashnum] = 83;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('I') == true){
     prehash[hashnum] = 13;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('J') == true){
     prehash[hashnum] = 46;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('K') == true){
     prehash[hashnum] = 29;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('L') == true){
     prehash[hashnum] = 26;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('M') == true){
     prehash[hashnum] = 269;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('N') == true){
     prehash[hashnum] = 71;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('O') == true){
     prehash[hashnum] = 110;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('P') == true){
     prehash[hashnum] = 187;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('Q') == true){
     prehash[hashnum] = 191;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('R') == true){
     prehash[hashnum] = 152;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('S') == true){
     prehash[hashnum] = 177;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('T') == true){
     prehash[hashnum] = 49;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('U') == true){
     prehash[hashnum] = 121;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('V') == true){
     prehash[hashnum] = 159;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('W') == true){
     prehash[hashnum] = 147;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('X') == true){
     prehash[hashnum] = 112;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('Y') == true){
     prehash[hashnum] = 114;
-    hashnum = prehash.length;
+    hashnum ++;
   }
   if (array2.includes('Z') == true){
     prehash[hashnum] = 130;
-    hashnum = prehash.length;
+    hashnum ++;
   }
-  return prehash;
+  if (array2.includes('1') == true){
+    prehash[hashnum] = 162;
+    hashnum ++;
+  }
+  if (array2.includes('2') == true){
+    prehash[hashnum] = 173;
+    hashnum ++;
+  }
+  if (array2.includes('3') == true){
+    prehash[hashnum] = 197;
+    hashnum ++;
+  }
+  if (array2.includes('4') == true){
+    prehash[hashnum] = 169;
+    hashnum ++;
+  }
+  if (array2.includes('5') == true){
+    prehash[hashnum] = 181;
+    hashnum ++;
+  }
+  if (array2.includes('6') == true){
+    prehash[hashnum] = 237;
+    hashnum ++;
+  }
+  if (array2.includes('7') == true){
+    prehash[hashnum] = 202;
+    hashnum ++;
+  }
+  if (array2.includes('8') == true){
+    prehash[hashnum] = 208;
+    hashnum ++;
+  }if (array2.includes('9') == true){
+    prehash[hashnum] = 219;
+    hashnum ++;
+  }
+  var hash = multiply(prehash);
+  var temp = prehash.length;
+  hash += prehash[1];
+  hash += prehash[4];
+  hash += prehash[8];
+  hash += temp;
+  return hash;
 }
