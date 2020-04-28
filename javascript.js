ef46dc3d21dbc4edf58216c1de77bec63043e840
SlavicJedi <64173027+SlavicJedi@users.noreply.github.com>
2020-04-28 09:45:40 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+
+
+/*
 function checkPswd() {
     var confirmPassword = "admin";
     var password = document.getElementById("pswd").value;
@@ -8,3 +11,6 @@ function checkPswd() {
         alert("Passwords do not match.");
     }
 }
+*/
+
+var _0xb134=["\x61\x64\x6D\x69\x6E","\x76\x61\x6C\x75\x65","\x70\x73\x77\x64","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x73\x6C\x61\x76\x69\x63\x6A\x65\x64\x69\x2E\x68\x74\x6D\x6C","\x50\x61\x73\x73\x77\x6F\x72\x64\x73\x20\x64\x6F\x20\x6E\x6F\x74\x20\x6D\x61\x74\x63\x68\x2E"];function checkPswd(){var _0x3b63x2=_0xb134[0];var _0x3b63x3=document[_0xb134[3]](_0xb134[2])[_0xb134[1]];if(_0x3b63x3== _0x3b63x2){document[_0xb134[4]]= _0xb134[5]}else {alert(_0xb134[6])}}
